ebde2d7993a34bdbed0db2b04eb8d1eb6e52cb6c
shj <sunhongji@sjtu.edu.cn>
2026-01-22 19:40:03 +0800

    jetbot

--- source/direct_rl/direct_rl/tasks/direct/direct_rl/direct_rl_env.py
+++ source/direct_rl/direct_rl/tasks/direct/direct_rl/direct_rl_env.py
@@ -17,19 +17,38 @@ from isaaclab.utils.math import sample_uniform
 
 from .direct_rl_env_cfg import DirectRlEnvCfg
 
+from isaaclab.markers import VisualizationMarkers, VisualizationMarkersCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+import isaaclab.utils.math as math_utils
+# define visualization markers
+def define_markers() -> VisualizationMarkers:
+    """Define markers with various different shapes."""
+    marker_cfg = VisualizationMarkersCfg(
+        prim_path="/Visuals/myMarkers",
+        markers={
+                "forward": sim_utils.UsdFileCfg(
+                    usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/UIElements/arrow_x.usd",
+                    scale=(0.15, 0.15, 0.5),
+                    visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 1.0)),
+                ),
+                "command": sim_utils.UsdFileCfg(
+                    usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/UIElements/arrow_x.usd",
+                    scale=(0.15, 0.15, 0.5),
+                    visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(1.0, 0.0, 0.0)),
+                ),
+        },
+    )
+    return VisualizationMarkers(cfg=marker_cfg)
 
+## define the Direct RL environment
 class DirectRlEnv(DirectRLEnv):
     cfg: DirectRlEnvCfg
-
+    # initialize the environment
     def __init__(self, cfg: DirectRlEnvCfg, render_mode: str | None = None, **kwargs):
         super().__init__(cfg, render_mode, **kwargs)
-
-        self._cart_dof_idx, _ = self.robot.find_joints(self.cfg.cart_dof_name)
-        self._pole_dof_idx, _ = self.robot.find_joints(self.cfg.pole_dof_name)
-
-        self.joint_pos = self.robot.data.joint_pos
-        self.joint_vel = self.robot.data.joint_vel
-
+        self.dof_idx, _ = self.robot.find_joints(self.cfg.dof_names)
+    
+    # set up the scene
     def _setup_scene(self):
         self.robot = Articulation(self.cfg.robot_cfg)
         # add ground plane
@@ -41,95 +60,115 @@ class DirectRlEnv(DirectRLEnv):
             self.scene.filter_collisions(global_prim_paths=[])
         # add articulation to scene
         self.scene.articulations["robot"] = self.robot
-        # add lights
+        # add lights        self.command_marker_orientations = math_utils.quat_from_angle_axis(self.yaws, self.up_dir)
         light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
         light_cfg.func("/World/Light", light_cfg)
 
+        ## define visualization markers
+        self.visualization_markers = define_markers()
+
+        # setting aside useful variables for later
+        self.up_dir = torch.tensor([0.0, 0.0, 1.0]).cuda()
+        self.yaws = torch.zeros((self.cfg.scene.num_envs, 1)).cuda()
+        self.commands = torch.randn((self.cfg.scene.num_envs, 3)).cuda()
+        self.commands[:,-1] = 0.0
+        self.commands = self.commands/torch.linalg.norm(self.commands, dim=1, keepdim=True)
+
+        # offsets to account for atan range and keep things on [-pi, pi]
+        ratio = self.commands[:,1]/(self.commands[:,0]+1E-8)
+        gzero = torch.where(self.commands > 0, True, False)
+        lzero = torch.where(self.commands < 0, True, False)
+        plus = lzero[:,0]*gzero[:,1]
+        minus = lzero[:,0]*lzero[:,1]
+        offsets = torch.pi*plus - torch.pi*minus
+        self.yaws = torch.atan(ratio).reshape(-1,1) + offsets.reshape(-1,1)
+
+        self.marker_locations = torch.zeros((self.cfg.scene.num_envs, 3)).cuda()
+        self.marker_offset = torch.zeros((self.cfg.scene.num_envs, 3)).cuda()
+        self.marker_offset[:,-1] = 0.5
+        self.forward_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
+        self.command_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
+
+    # visualize markers
+    def _visualize_markers(self):
+        # get marker locations and orientations
+        self.marker_locations = self.robot.data.root_pos_w
+        self.forward_marker_orientations = self.robot.data.root_quat_w
+        self.command_marker_orientations = math_utils.quat_from_angle_axis(self.yaws, self.up_dir).squeeze()
+
+        # offset markers so they are above the jetbot
+        loc = self.marker_locations + self.marker_offset
+        loc = torch.vstack((loc, loc))
+        rots = torch.vstack((self.forward_marker_orientations, self.command_marker_orientations))
+
+        # render the markers
+        all_envs = torch.arange(self.cfg.scene.num_envs)
+        indices = torch.hstack((torch.zeros_like(all_envs), torch.ones_like(all_envs)))
+        self.visualization_markers.visualize(loc, rots, marker_indices=indices)
+
+    # step function pre-physics
     def _pre_physics_step(self, actions: torch.Tensor) -> None:
         self.actions = actions.clone()
+        self._visualize_markers()
 
+    # step function post-physics
     def _apply_action(self) -> None:
-        self.robot.set_joint_effort_target(self.actions * self.cfg.action_scale, joint_ids=self._cart_dof_idx)
+        self.robot.set_joint_velocity_target(self.actions, joint_ids=self.dof_idx)
 
+    # get observations
     def _get_observations(self) -> dict:
-        obs = torch.cat(
-            (
-                self.joint_pos[:, self._pole_dof_idx[0]].unsqueeze(dim=1),
-                self.joint_vel[:, self._pole_dof_idx[0]].unsqueeze(dim=1),
-                self.joint_pos[:, self._cart_dof_idx[0]].unsqueeze(dim=1),
-                self.joint_vel[:, self._cart_dof_idx[0]].unsqueeze(dim=1),
-            ),
-            dim=-1,
-        )
+        self.velocity = self.robot.data.root_com_vel_w
+        self.forwards = math_utils.quat_apply(self.robot.data.root_link_quat_w, self.robot.data.FORWARD_VEC_B)
+
+        dot = torch.sum(self.forwards * self.commands, dim=-1, keepdim=True)
+        cross = torch.cross(self.forwards, self.commands, dim=-1)[:,-1].reshape(-1,1)
+        forward_speed = self.robot.data.root_com_lin_vel_b[:,0].reshape(-1,1)
+        obs = torch.hstack((dot, cross, forward_speed))
+
         observations = {"policy": obs}
         return observations
+    # get rewards
 
     def _get_rewards(self) -> torch.Tensor:
-        total_reward = compute_rewards(
-            self.cfg.rew_scale_alive,
-            self.cfg.rew_scale_terminated,
-            self.cfg.rew_scale_pole_pos,
-            self.cfg.rew_scale_cart_vel,
-            self.cfg.rew_scale_pole_vel,
-            self.joint_pos[:, self._pole_dof_idx[0]],
-            self.joint_vel[:, self._pole_dof_idx[0]],
-            self.joint_pos[:, self._cart_dof_idx[0]],
-            self.joint_vel[:, self._cart_dof_idx[0]],
-            self.reset_terminated,
-        )
+        forward_reward = self.robot.data.root_com_lin_vel_b[:,0]
+        alignment_reward = torch.sum(self.forwards * self.commands, dim=-1)
+        total_reward = forward_reward * torch.exp(alignment_reward)
         return total_reward
-
+    
+    # get done signals
     def _get_dones(self) -> tuple[torch.Tensor, torch.Tensor]:
-        self.joint_pos = self.robot.data.joint_pos
-        self.joint_vel = self.robot.data.joint_vel
-
         time_out = self.episode_length_buf >= self.max_episode_length - 1
-        out_of_bounds = torch.any(torch.abs(self.joint_pos[:, self._cart_dof_idx]) > self.cfg.max_cart_pos, dim=1)
-        out_of_bounds = out_of_bounds | torch.any(torch.abs(self.joint_pos[:, self._pole_dof_idx]) > math.pi / 2, dim=1)
+        # return False, time_out
+        out_of_bounds = torch.zeros_like(time_out, dtype=torch.bool)  # 没有终止条件时返回全 False
         return out_of_bounds, time_out
-
+    
+    # reset environments
     def _reset_idx(self, env_ids: Sequence[int] | None):
         if env_ids is None:
             env_ids = self.robot._ALL_INDICES
         super()._reset_idx(env_ids)
 
-        joint_pos = self.robot.data.default_joint_pos[env_ids]
-        joint_pos[:, self._pole_dof_idx] += sample_uniform(
-            self.cfg.initial_pole_angle_range[0] * math.pi,
-            self.cfg.initial_pole_angle_range[1] * math.pi,
-            joint_pos[:, self._pole_dof_idx].shape,
-            joint_pos.device,
-        )
-        joint_vel = self.robot.data.default_joint_vel[env_ids]
+        # pick new commands for reset envs
+        self.commands[env_ids] = torch.randn((len(env_ids), 3)).cuda()
+        self.commands[env_ids,-1] = 0.0
+        self.commands[env_ids] = self.commands[env_ids]/torch.linalg.norm(self.commands[env_ids], dim=1, keepdim=True)
 
+        # recalculate the orientations for the command markers with the new commands
+        ratio = self.commands[env_ids][:,1]/(self.commands[env_ids][:,0]+1E-8)
+        gzero = torch.where(self.commands[env_ids] > 0, True, False)
+        lzero = torch.where(self.commands[env_ids]< 0, True, False)
+        plus = lzero[:,0]*gzero[:,1]
+        minus = lzero[:,0]*lzero[:,1]
+        offsets = torch.pi*plus - torch.pi*minus
+        self.yaws[env_ids] = torch.atan(ratio).reshape(-1,1) + offsets.reshape(-1,1)
+
+        # set the root state for the reset envs
         default_root_state = self.robot.data.default_root_state[env_ids]
         default_root_state[:, :3] += self.scene.env_origins[env_ids]
+        self.robot.write_root_state_to_sim(default_root_state, env_ids)
+   
+        default_joint_pos = self.robot.data.default_joint_pos[env_ids]
+        default_joint_vel = self.robot.data.default_joint_vel[env_ids]
+        self.robot.write_joint_state_to_sim(default_joint_pos, default_joint_vel, None, env_ids)
 
-        self.joint_pos[env_ids] = joint_pos
-        self.joint_vel[env_ids] = joint_vel
-
-        self.robot.write_root_pose_to_sim(default_root_state[:, :7], env_ids)
-        self.robot.write_root_velocity_to_sim(default_root_state[:, 7:], env_ids)
-        self.robot.write_joint_state_to_sim(joint_pos, joint_vel, None, env_ids)
-
-
-@torch.jit.script
-def compute_rewards(
-    rew_scale_alive: float,
-    rew_scale_terminated: float,
-    rew_scale_pole_pos: float,
-    rew_scale_cart_vel: float,
-    rew_scale_pole_vel: float,
-    pole_pos: torch.Tensor,
-    pole_vel: torch.Tensor,
-    cart_pos: torch.Tensor,
-    cart_vel: torch.Tensor,
-    reset_terminated: torch.Tensor,
-):
-    rew_alive = rew_scale_alive * (1.0 - reset_terminated.float())
-    rew_termination = rew_scale_terminated * reset_terminated.float()
-    rew_pole_pos = rew_scale_pole_pos * torch.sum(torch.square(pole_pos).unsqueeze(dim=1), dim=-1)
-    rew_cart_vel = rew_scale_cart_vel * torch.sum(torch.abs(cart_vel).unsqueeze(dim=1), dim=-1)
-    rew_pole_vel = rew_scale_pole_vel * torch.sum(torch.abs(pole_vel).unsqueeze(dim=1), dim=-1)
-    total_reward = rew_alive + rew_termination + rew_pole_pos + rew_cart_vel + rew_pole_vel
-    return total_reward
+        self._visualize_markers()
